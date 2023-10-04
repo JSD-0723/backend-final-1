d14250bca9230768cec7d5d02b39b220787631f7
abdallahaltanna <abdallahmyaltanna@gmail.com>
2023-10-04 16:56:35 +0300

    Create test endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import morgan from 'morgan';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 
@@ -14,10 +15,20 @@ const server = http.createServer(app);
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(compression());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.disable('x-powered-by');
 
 app.set('port', port);
+
+app.get('/api/v1/test-endpoint', (req, res) => {
+  res.send('Hello frontend team!');
+});
 
 export { app, server };
